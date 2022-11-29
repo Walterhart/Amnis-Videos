23c5f97812686be60d2ad4019e9d8113fba32694
Walterhart <70658679+Walterhart@users.noreply.github.com>
2022-11-29 04:03:15 -0500

    fix recommend

--- src/VideoComponents/VideoDetails.js
+++ src/VideoComponents/VideoDetails.js
@@ -1,13 +1,24 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { supabase } from "../config/supabaseClient";
+import { AiOutlineStar } from "react-icons/ai";
+import { AiFillStar } from "react-icons/ai";
+import useFetchFavorite from "./useFetchFavorite";
+import FavoriteControl from "./FavortieControl.";
+import useFetchUser from "./useFetchUser";
+import RecocomendByDirector from "./ReccomendByDirector";
+
 
 const VideoDetail = ({platform}) => {
     const [video,setVideo] = useState('');
     const [casts,setCasts] = useState('');
     const [directors,setDirectors] = useState('');
+    const {user} = useFetchUser('')
+   
     const navigate = useNavigate()
     const { id } = useParams()
+
+
     useEffect(()=>{
         const fetchVideos =async() =>{
             const { data, error } = await supabase
@@ -45,6 +56,7 @@ const VideoDetail = ({platform}) => {
                  }
                 if (data){
                     setDirectors(data)  
+                    
                 }
                 
         }
@@ -54,6 +66,8 @@ const VideoDetail = ({platform}) => {
         fetchCasts()
         
     },[id, navigate,platform])
+
+
     return ( 
         <div className="video-details">
             <h1>{video.title}</h1>
@@ -61,8 +75,7 @@ const VideoDetail = ({platform}) => {
             <h3> {video.age_certification}</h3>
             <h3>Genre: {video.genres} </h3>
             <h3>Description:</h3>
-            <p> {video.description}</p>
-            
+            <p> {video.description}</p> 
             <h3> Release year: </h3>
             <p>{video.release_year}</p>
             <p>{video.production_countries}</p>
@@ -70,19 +83,37 @@ const VideoDetail = ({platform}) => {
             {casts && casts.map((cast) => 
             <div className="cast" key={cast.id}>
                 <p> {cast.name} as {cast.character}</p>
-            </div>  
+            </div>          
             )}
             <h3>Director:</h3>
+           
             {directors && directors.map((director) => 
-            <div className="cast" key={director.id}>
-                
+            <div className="cast" key={director.id}> 
                 <p> {director.name}  </p>
             </div>
 
             
             )}
-
+            {user && user!==null ?
+            <>
+             
+             <FavoriteControl  id ={id} platform={platform} user={user}/> 
+             </>
+             :
+             <>
+                <h3>Please login for other feature</h3>
+             </>
+            }
+            
+            {directors && directors!== null && directors.map((director) => (
+            
+            <RecocomendByDirector key={director.id} director={director}  platform={platform}/> 
         
+            
+          ))}
+            
+            
+           
         </div>
      )
 }
